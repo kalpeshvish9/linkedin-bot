5ff3456461fcec1197d842f201c065e9dc6d5fb9
kalpeshvish9 <kalpeshvish9gmail.com>
2025-01-26 13:49:09 +0530

    beta-untested-update-outdated-elements

--- login.py
+++ login.py
@@ -1,12 +1,13 @@
 from dependencies import *
 from session_manager import get_session
+from selenium.webdriver.common.by import By
 
 driver = get_session()
 
 driver = webdriver.Chrome()
 driver.get("https://www.linkedin.com")
 
-username = driver.find_element_by_id("session_key")
+username = driver.find_element(By.ID, "session_key")
 username.send_keys(config.username)
 password = driver.find_element_by_id("session_password")
 password.send_keys(config.password)
